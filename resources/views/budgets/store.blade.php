8db1dfc2d22eca7843a99ec9976fcb780d793652
thejunghare <prasadjunghare2@gmail.com>
2024-01-05 23:39:50 +0530

    redesign form

--- resources/views/budgets/store.blade.php
+++ resources/views/budgets/store.blade.php
@@ -7,29 +7,38 @@
 
         <ol class="breadcrumb mb-4">
             <li class="breadcrumb-item active">Features</li>
+
+
         </ol>
         <!-- form -->
         <form class="py-4 " method="post" action="/b" id="addbudgetform">
             @csrf
-            <div class="mb-3">
-                <label for="basic-url" class="form-label">Set Budget</label>
-                <div class="input-group mb-3">
+
+            <div class="row g-3">
+                <div class="col input-group mb-3">
                     <!-- date -->
+                    <span class="input-group-text" id="basic-addon1">
+                    <i class="fa-solid fa-calendar-days"></i>
+                    </span>
                     <input type="date" class="form-control" required name="date" placeholder="Date"
                            aria-label="Username">
-                    <span class="input-group-text">₹</span>
+                </div>
+                <div class="col input-group mb-3">
                     <!-- amount -->
+                    <span class="input-group-text" id="basic-addon1">
+                    <i class="fa-solid fa-indian-rupee-sign"></i>
+                    </span>
                     <input id="amount" type="amount" class="form-control @error('amount') is-invalid @enderror"
                            name="amount" value="{{ old('amount') }}" autocomplete="amount" autofocus>
-                    <span class="input-group-text">.00</span>
+
                     @error('amount')
                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                     @enderror
                 </div>
-                <p class="text-danger fw-normal mark small d-inline" id="displayerror"></p>
             </div>
+
             <button type="submit" class="btn btn-primary" id="addbudget">Add budget</button>
         </form>
     </div>
