2246331cfba6a5cd71239ae7a24d61080e0e4c75
thejunghare <prasadjunghare2@gmail.com>
2024-01-08 16:39:24 +0530

    changed font to work sans

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -20,4 +20,9 @@
     <link href="/css/styles.css" rel="stylesheet"/>
     <!-- fontawesome icons -->
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+
+    {{-- work sans --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
