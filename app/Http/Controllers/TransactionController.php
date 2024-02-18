d066327b6dc95f65e674cbe0c530cbb4e0c6080e
thejunghare <prasadjunghare2@gmail.com>
2024-02-18 14:11:08 +0530

    fix store daily transactions

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Transaction;
 use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
@@ -23,34 +24,26 @@ class TransactionController extends Controller
         return view("transactions.show", compact("transactions"));
     }
 
-    public function store()
+    public function store(Request $request): RedirectResponse
     {
-        $data = request()->validate([
-            'type' => 'required',
+        $data = $request->validate([
+            'transaction_type_id' => 'required',
             'date' => 'required',
             'amount' => 'required',
-            'category' => 'required',
-            'paymode' => 'required'
+            'category_id' => 'required',
+            'paymode_id' => 'required'
         ]);
 
-        //dd(request()->all());
+        $data['user_id'] = Auth::id();
 
-        auth()->user()->transactions()->create([
-            'type' => $data['type'],
-            'date' => $data['date'],
-            'amount' => $data['amount'],
-            'category' => $data['category'],
-            'paymode' => $data['paymode'],
-        ]);
-
+        Transaction::create($data);
 
         return redirect('/t')->with([
             'success' => 'Transaction added successfully!'
         ]);
-
     }
 
-    // todo: see single transaction
+
     public function show($transaction)
     {
         $getTransaction = Transaction::query()
@@ -61,6 +54,26 @@ class TransactionController extends Controller
         // dd($getId, Auth::user());
 
 
+        /*
+
+        $transactions = GroupBudgetTransaction::select(
+            'group_budget_transactions.*',
+            'pay_mode.paymode_type',
+            'default_categories.category_name',
+            'default_category_types.category_type',
+            'group_budget_transactions.amount'
+        )
+            ->join('default_categories', 'group_budget_transactions.category_id', '=', 'default_categories.id')
+            ->join('default_category_types', 'group_budget_transactions.transaction_type_id', '=', 'default_category_types.id')
+            ->join('group_budgets', 'group_budget_transactions.for_budget_id', '=', 'group_budgets.id')
+            ->join('pay_mode', 'group_budget_transactions.paymode_id', '=', 'pay_mode.id')
+            ->where('group_budget_transactions.for_budget_id', $id)
+            ->where('group_budget_transactions.user_id', $userID)
+            ->paginate(5);
+
+        */
+
+
         if (!$getTransaction) {
             return response()->json(['ID not found'], 404);
         }
@@ -69,7 +82,7 @@ class TransactionController extends Controller
         return view('transactions.see', compact('getTransaction'));
     }
 
-    // todo: edit single transaction
+
     public function edit($transaction)
     {
         $getTransaction = Transaction::query()
@@ -88,7 +101,7 @@ class TransactionController extends Controller
         return view('transactions.edit', compact('getTransaction'));
     }
 
-    // todo: update single tarnasction
+
     public function update(Request $request, $id)
     {
         $item = Transaction::find($id);
@@ -104,7 +117,7 @@ class TransactionController extends Controller
         ]);
     }
 
-    //todo: delete budget from db
+
     public function destroy($transactions)
     {
         $getTransaction = Transaction::query()
--- resources/views/transactions/store.blade.php
+++ resources/views/transactions/store.blade.php
@@ -12,7 +12,91 @@
             <form class="" method="post" action="/t">
                 @csrf
 
-                @livewire('dynamic-select')
+                {{-- type of transaction --}}
+                <div class="d-flex align-items-center justify-content-between mb-3">
+                    {{-- Expense --}}
+                    <div class="form-check">
+                        <input class="form-check-input @error('transaction_type_id') is-invalid @enderror" type="radio"
+                            name="transaction_type_id" id="expense" value="2" checked
+                            autocomplete="transaction_type_id" autofocus>
+                        <label class="form-check-label" for="expense">
+                            Expense
+                        </label>
+                        @error('transaction_type_id')
+                            <span role="alert" class="invalid-feedback">
+                                <strong>
+                                    {{ $message }}
+                                </strong>
+                            </span>
+                        @enderror
+                    </div>
+                    {{-- Income --}}
+                    <div class="form-check">
+                        <input class="form-check-input @error('transaction_type_id') is-invalid @enderror" type="radio"
+                            name="transaction_type_id" id="income" value="1" autocomplete="transaction_type_id"
+                            autofocus>
+                        <label class="form-check-label" for="income">
+                            Income
+                        </label>
+                        @error('transaction_type_id')
+                            <span role="alert" class="invalid-feedback">
+                                <strong>
+                                    {{ $message }}
+                                </strong>
+                            </span>
+                        @enderror
+                    </div>
+                    {{-- Transfer --}}
+                    <div class="form-check">
+                        <input class="form-check-input @error('transaction_type_id') is-invalid @enderror " type="radio"
+                            name="transaction_type_id" id="transfer" value="3" disabled
+                            autocomplete="transaction_type_id" autofocus>
+                        <label class="form-check-label" for="transfer">
+                            Transfer
+                        </label>
+                        @error('transaction_type_id')
+                            <span role="alert" class="invalid-feedback">
+                                <strong>
+                                    {{ $message }}
+                                </strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="row g-3 mb-3">
+                    {{-- category --}}
+                    <div class="col">
+                        <select id="category-options" class="form-control @error('category-options') is-invalid @enderror"
+                            aria-label="Default select example" name="category_id" required autofocus
+                            value="{{ old('category_id') }}" autocomplete="category-options">
+                            <option value="" disabled selected>Select category</option>
+                        </select>
+                        @error('category-options')
+                            <span role="alert" class="invalid-feedback">
+                                <strong>
+                                    {{ $message }}
+                                </strong>
+                            </span>
+                        @enderror
+                    </div>
+                    {{-- paymode --}}
+                    <div class="col">
+                        <select id="paymode-options" class="form-control @error('paymode-options') is-invalid @enderror"
+                            aria-label="Default select example" name="paymode_id" required autofocus
+                            value="{{ old('paymode_id') }}" autocomplete="paymode-options">
+                            <option value="" disabled selected>Select payment mode</option>
+                        </select>
+                        @error('paymode-options')
+                            <span role="alert" class="invalid-feedback">
+                                <strong>
+                                    {{ $message }}
+                                </strong>
+                            </span>
+                        @enderror
+                    </div>
+
+                </div>
 
                 <div class="row g-3 mb-3">
                     {{-- date of transaction --}}
@@ -32,5 +116,50 @@
                 <button type="submit" class="btn btn-primary">Save</button>
             </form>
         </div>
+
+
+        <script>
+            $(document).ready(function() {
+                var paymodeSelectOption = $('#paymode-options');
+                var categorySelectOption = $('#category-options');
+
+                $.ajax({
+                    url: '/paymode-options',
+                    method: 'GET',
+                    success: function(data) {
+                        paymodeSelectOption.empty();
+                        paymodeSelectOption.append(
+                            '<option value="" disabled selected>Select payment mode</option>');
+
+                        $.each(data, function(index, option) {
+                            paymodeSelectOption.append('<option value="' + option.id + '">' + option
+                                .paymode_type + '</option>');
+                        });
+                    },
+                    error: function(error) {
+                        console.error('Error fetching payment options:', error);
+                    }
+                });
+
+                $.ajax({
+                    url: '/categories-options',
+                    method: 'GET',
+                    success: function(data) {
+                        categorySelectOption.empty();
+                        categorySelectOption.append(
+                            '<option value="" disabled selected>Select category mode</option>');
+
+                        $.each(data, function(index, option) {
+                            categorySelectOption.append('<option value="' + option.id + '">' +
+                                option
+                                .category_name + '</option>');
+                        });
+                    },
+                    error: function(error) {
+                        console.error('Error fetching payment options:', error);
+                    }
+                });
+            });
+        </script>
     </main>
 @endsection
